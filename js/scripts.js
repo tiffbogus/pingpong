7bab26e10cda122bcaa7a02b96e3a74d0cf70ddf
Tiffany Bogus <tiffbogus@Master-Control.local>
2015-10-16 22:23:47 -0700

    Add css files, add script coding for list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
   var number = parseInt(prompt("What number should I ping-pong up to?"))
 
+  for(n = 1; n <= number; n += 1) {
+    if (n % 15 === 0) {
+      $("#numlist").append("<li>" + "ping-pong!" + "</li>");
+    } else if (n % 5 === 0) {
+      $("#numlist").append("<li>" + "pong!" + "</li>");
+    } else if (n % 3 === 0) {
+      $("#numlist").append("<li>" + "ping!" + "</li>");
+    } else {
+      $("#numlist").append("<li>" + n + "</li>");
+    }
+
+  }
 
 });
